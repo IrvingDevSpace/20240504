fdd4df1de115a53a350f7321f38724a4e1f77c11
周政輝 <leo604leo604@gmail.com>
2024-06-01 19:53:55 +0800

    新增使用者輸入進行資料建檔

--- TT/Program.cs
+++ TT/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TT.Models;
 
 namespace TT
 {
@@ -10,6 +11,16 @@ namespace TT
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("請輸入姓名");
+            String Name = Console.ReadLine();
+            Console.WriteLine("請輸入地址");
+            String Address = Console.ReadLine();
+            Console.WriteLine("請輸入帳號：");
+            String Account = Console.ReadLine();
+            Console.WriteLine("請輸入密碼：");
+            String Password = Console.ReadLine();
+
+            User user = new User(Name, Address, Account, Password);
             
         }
     }
