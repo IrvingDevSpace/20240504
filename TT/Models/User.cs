d4e69024ee1748d8c455dc361239a5d1a951af16
周政輝 <leo604leo604@gmail.com>
2024-06-01 19:23:01 +0800

    Add User Model

--- TT/Models/User.cs
+++ TT/Models/User.cs
@@ -1,7 +1,20 @@
+using System;
+
 namespace TT.Models
 {
     public class User
     {
-        
+        public String Name { get; set; }
+        public String Address { get; set; }
+        public String Account { get; set; }
+        public String Password { get; set; }
+
+        public User(string name, string address, string account, string password)
+        {
+            Name = name;
+            Address = address;
+            Account = account;
+            Password = password;
+        }
     }
 }
