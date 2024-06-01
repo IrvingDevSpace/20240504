8909c71d56f12a99d64f1621f32afb206c827fdf
周政輝 <leo604leo604@gmail.com>
2024-06-01 19:37:29 +0800

    Add CheckAccountIsExist

--- TT/Services/RegisterService.cs
+++ TT/Services/RegisterService.cs
@@ -1,7 +1,16 @@
+using TT.Models;
+
 namespace TT.Services
 {
     public class RegisterService
     {
-        
+        public bool CheckAccountIsExist(User user)
+        {
+            if (user.Account == "Leo" && user.Password == "1234")
+                return false;
+
+            return true;
+            
+        }
     }
 }
