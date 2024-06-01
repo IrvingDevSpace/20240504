f02b49da0282450e94ec4966304607cf137f50d2
GRAM\IRVING <qaz880725@gmail.com>
2024-06-01 19:43:14 +0800

    1.新增Login功能 2.新增ForgetPassword功能

---
+++ TT/Services/LoginService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TT.Models;
+
+namespace TT.Services
+{
+    internal class LoginService
+    {
+        public static bool Login(User user)
+        {
+            if (user == null)
+                return false;
+            if (String.IsNullOrEmpty(user.Account) || String.IsNullOrEmpty(user.Password))
+                return false;
+            return true;
+        }
+
+        public static bool ForgetPassword(User user)
+        {
+            if (String.IsNullOrEmpty(user.Address))
+                return false;
+            return true;
+        }
+    }
+}
